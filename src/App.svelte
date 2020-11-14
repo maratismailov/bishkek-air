aaf01e019764c2685e9efd2240572215266cf5d3
Marat Ismailov <marat.ismailov889@gmail.com>
2020-11-15 01:02:46 +0600

    Changed data to daily

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,9 @@
 <script>
-  import { Route, active } from "tinro";
-  import History from "./components/History.svelte";
+  import { Route } from "tinro";
   import CurrentDust from "./components/CurrentDust.svelte";
 </script>
-
-<style>
-
-</style>
-
-<main>
-  <div class="scrollmenu">
-    <a href="/" use:active exact>Home</a>
-    <a href="/history" use:active>History</a>
-  </div>
-</main>
 
 <Route path="/">
   <CurrentDust />
 </Route>
-<Route path="/history">
-  <History />
-</Route>
+
--- src/components/CurrentDust.svelte
+++ src/components/CurrentDust.svelte
@@ -1,18 +1,11 @@
 <script>
   import L from "leaflet";
-  import axios from "axios";
-
-  // import "leaflet/dist/leaflet.css";
-  // import "leaflet.markercluster/dist/MarkerCluster.css";
-  // import "leaflet.markercluster/dist/MarkerCluster.Default.css";
-  // import "leaflet/dist/images/marker-icon-2x.png";
-  // import "leaflet/dist/images/marker-shadow.png";
+  import axios from "redaxios";
   import { onMount } from "svelte";
-  // import "leaflet/dist/leaflet.css";
   let height = (window.innerHeight * 0.93).toString() + "px";
   let map;
-  let records;
   let data;
+  let today_data;
 
   export let center = [42.87, 74.594];
   export let zoom = 12;
@@ -20,57 +13,47 @@
   onMount(() => {
     createMap();
     axios
-      .get("https://krakenflask.herokuapp.com/readcurrentdata")
-      .then(response => {
-        data = response.data;
-        refineData(data);
+      .get(
+        "https://x1cz0kcbm8.execute-api.eu-central-1.amazonaws.com/?method=getfromjsonnetunique"
+      )
+      .then((response) => {
+        data = response.data.data;
+        // refineData(data);
+        put_unique_points(data);
       });
   });
-  const refineData = data => {
-    const refined_data = data.filter(
-      elem =>
-        elem.sensordatavalues[0].value_type != "temperature" &&
-        elem.sensordatavalues[0].value_type != "humidity"
-    );
-    refined_data.forEach(elem => {
-      const filtered = elem.sensordatavalues.filter(
-        elem => elem.value_type == "P2"
-      );
-      elem.sensordatavalues = filtered[0];
-    });
-    const pre_data_to_display = refined_data.map(elem => {
-      const data = {
-        id: elem.sensor.id,
-        latitude: elem.location.latitude,
-        longitude: elem.location.longitude,
-        value: parseFloat(elem.sensordatavalues.value)
-          .toFixed(2)
-          .toString()
-      };
-      return data;
-    });
-    var data_to_display = pre_data_to_display.filter(function(
-      obj,
-      index,
-      self
-    ) {
-      return (
-        index ===
-        self.findIndex(function(t) {
-          return t["id"] === obj["id"];
+  const put_unique_points = (data) => {
+    axios
+      .get(
+        "https://x1cz0kcbm8.execute-api.eu-central-1.amazonaws.com/?method=getfromjsonnetdaily"
+      )
+      .then((response) => {
+        today_data = response.data.data;
+        data.map(elem =>{
+          elem.value = ''
         })
-      );
-    });
-    data_to_display.forEach(elem => {
+        today_data.map((elem) => {
+          data.map((data_elem) => {
+            if (data_elem.id == elem.id) {
+              data_elem.value = elem.value
+            }
+          });
+        });
+        place_markers();
+      });
+  };
+
+  const place_markers = () => {
+    data.forEach((elem) => {
       const numberIcon = L.divIcon({
         className: "number-icon",
         iconSize: [35, 35],
         // iconAnchor: [10, 44],
         popupAnchor: [3, -40],
-        html: elem.value
+        html: elem.value,
       });
-      L.marker([parseFloat(elem.latitude), parseFloat(elem.longitude)], {
-        icon: numberIcon
+      L.marker([parseFloat(elem.lat), parseFloat(elem.lon)], {
+        icon: numberIcon,
       }).addTo(map);
     });
     map.setView(center, zoom);
@@ -84,22 +67,13 @@
         attribution:
           'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
-        // id: "mapbox/streets-v11",
         id: "mapbox/dark-v10",
         tileSize: 512,
         zoomOffset: -1,
         accessToken:
-          "pk.eyJ1IjoicmVnZmU4OSIsImEiOiJjazlvY2oxdW0wOG5tM2RtcmV0eGRuZDk4In0.WIsrA9dq-v_Yw5gX_D17qg"
+          "pk.eyJ1IjoicmVnZmU4OSIsImEiOiJjazlvY2oxdW0wOG5tM2RtcmV0eGRuZDk4In0.WIsrA9dq-v_Yw5gX_D17qg",
       }
     ).addTo(map);
-    // L.tileLayer(
-    //   "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
-    //   {
-    //     maxZoom: 20,
-    //     attribution:
-    //       '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-    //   }
-    // ).addTo(map);
   };
 
   const resize = () => {
